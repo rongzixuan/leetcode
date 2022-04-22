4903beeaf27e7d37f2f94a6f65b01261c8e20cac
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-04-22 09:54:39 +0800

    Update and rename 0231.2的幂（简单） to 0231.2的幂（简单）.py

--- 0231.2的幂（简单）.py
+++ 0231.2的幂（简单）.py
@@ -2,6 +2,7 @@
 给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
 
 如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。
+
 """
 
 class Solution:
@@ -11,7 +12,7 @@ class Solution:
         # 方法一：迭代
         # 时间复杂度：O(logn)
         # 空间复杂度：O(1)
-        """if n <= 0:
+        if n <= 0:
             return False
         elif n == 1:
             return True
@@ -21,7 +22,7 @@ class Solution:
                 return False
             n //= 2
 
-        return True"""
+        return True
 
 
         # 方法二：位运算
