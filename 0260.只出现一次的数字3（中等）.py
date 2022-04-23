e69c9ef3c6277b249d47b70b8ca36d5ac884b748
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-04-23 19:33:23 +0800

    Update and rename 0260.只出现一次的数字3（中等） to 0260.只出现一次的数字3（中等）.py

--- 0260.只出现一次的数字3（中等）.py
+++ 0260.只出现一次的数字3（中等）.py
@@ -1,9 +1,8 @@
 """
 给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按 任意顺序 返回答案。
 
- 
-
 进阶：你的算法应该具有线性时间复杂度。你能否仅使用常数空间复杂度来实现？
+
 """
 
 class Solution:
@@ -13,7 +12,7 @@ class Solution:
         # 方法一：哈希表
         # 时间复杂度：O(n)
         # 空间复杂度：O(n)
-        """from collections import defaultdict
+        from collections import defaultdict
         hash_table = defaultdict(int)
 
         for num in nums:
@@ -24,7 +23,7 @@ class Solution:
             if v == 1:
                 res.append(k)
 
-        return res"""
+        return res
 
 
         # 方法二：位运算
