159c61841aaddfab4c484ae5173540c9e153ac97
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-04-25 09:33:12 +0800

    Update and rename 0300.最长递增子序列（中等） to 0300.最长递增子序列（中等）.py

--- 0300.最长递增子序列（中等）.py
+++ 0300.最长递增子序列（中等）.py
@@ -2,6 +2,7 @@
 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
 
 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+
 """
 
 class Solution(object):
@@ -17,15 +18,14 @@ class Solution(object):
         n = len(nums)
 
         # 方法一：动态规划
-        """dp = [1] * n
+        dp = [1] * n
         
         for i in range(n):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j]+1)
         # 返回 dp 数组最大值
-        return max(dp)"""
-
+        return max(dp)
 
 
         # 方法二：动态规划 + 贪心 + 二分
