373c40f601667bcffafa8381d6c47b1cf8a6e9b0
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-04-28 10:10:30 +0800

    Update and rename 0412.Fizz Buzz（简单） to 0412.Fizz Buzz（简单）.py

--- 0412.Fizz Buzz（简单）.py
+++ 0412.Fizz Buzz（简单）.py
@@ -2,9 +2,7 @@
 写一个程序，输出从 1 到 n 数字的字符串表示。
 
 1. 如果 n 是3的倍数，输出“Fizz”；
-
 2. 如果 n 是5的倍数，输出“Buzz”；
-
 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
 
 """
@@ -31,3 +29,7 @@ class Solution:
                 res.append(str(i+1))
 
         return res
+    
+    
+    
+    
