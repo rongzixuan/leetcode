87baa88de3d6abaf283c8c390682e04495cad09f
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-06 12:10:02 +0800

    Create 0933.最近的请求次数（简单）.py

---
+++ 0933.最近的请求次数（简单）.py
@@ -0,0 +1,51 @@
+"""
+写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+
+请你实现 RecentCounter 类：
+
+RecentCounter() 初始化计数器，请求数为 0 。
+int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+保证 每次对 ping 的调用都使用比之前更大的 t 值。
+
+ 
+
+示例 1：
+输入：
+["RecentCounter", "ping", "ping", "ping", "ping"]
+[[], [1], [100], [3001], [3002]]
+输出：
+[null, 1, 2, 3, 3]
+
+解释：
+RecentCounter recentCounter = new RecentCounter();
+recentCounter.ping(1);     // requests = [1]，范围是 [-2999,1]，返回 1
+recentCounter.ping(100);   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+recentCounter.ping(3001);  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回 3
+ 
+提示：
+1 <= t <= 10^9
+保证每次对 ping 调用所使用的 t 值都 严格递增
+至多调用 ping 方法 10^4 次
+
+"""
+
+
+# 方法一：队列
+# 时间复杂度：O(n)
+# 空间复杂度：O(n)
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = deque([])
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while t - self.queue[0] > 3000:
+            self.queue.popleft()
+        return len(self.queue)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
