906ef22d891d81a4ba286ee99c9dc391eb527e23
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-09 14:25:19 +0800

    Update and rename 0540.有序数组中的单一元素（中等） to 0540.有序数组中的单一元素（中等）.py

--- 0540.有序数组中的单一元素（中等）.py
+++ 0540.有序数组中的单一元素（中等）.py
@@ -5,7 +5,6 @@
 
 你设计的解决方案必须满足 O(log n) 时间复杂度和 O(1) 空间复杂度。
 
- 
 
 示例 1:
 输入: nums = [1,1,2,3,3,4,4,8,8]
@@ -15,7 +14,6 @@
 输入: nums =  [3,3,7,7,10,11,11]
 输出: 10
  
-
 提示:
 1 <= nums.length <= 10^5
 0 <= nums[i] <= 10^5
@@ -30,7 +28,7 @@ class Solution:
         # 方法一：二分法
         # 时间复杂度：O(logn)
         # 空间复杂度：O(1)
-        """n = len(nums)
+        n = len(nums)
         left, right = 0, n - 1
         while left <= right:
             mid = left + (right - left) // 2
@@ -53,7 +51,7 @@ class Solution:
                 if nums[mid] == nums[mid - 1]:              
                     left = mid + 1
                 else:
-                    right = mid - 1"""
+                    right = mid - 1
 
 
         # 方法二：二分法2
@@ -70,20 +68,6 @@ class Solution:
                 right = mid
 
         return nums[right]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
