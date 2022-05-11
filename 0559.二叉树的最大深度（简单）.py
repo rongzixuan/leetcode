82881ef7b937c4d4373bf8726fc6de2d7a2dd40c
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-11 19:31:49 +0800

    Update and rename 0559.二叉树的最大深度（简单） to 0559.二叉树的最大深度（简单）.py

--- 0559.二叉树的最大深度（简单）.py
+++ 0559.二叉树的最大深度（简单）.py
@@ -1,10 +1,7 @@
 """
 给定一个 N 叉树，找到其最大深度。
-
 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
-
 N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。
-
 
 示例 1：
 输入：root = [1,null,3,2,4,null,5,6]
@@ -13,7 +10,6 @@ N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（
 示例 2：
 输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
 输出：5
- 
 
 提示：
 树的深度不会超过 1000 。
@@ -36,7 +32,7 @@ class Solution:
         # 方法一：深度优先搜索(递归)
         # 时间复杂度：O(N)
         # 空间复杂度：O(N)
-        """def dfs(node):
+        def dfs(node):
             if not node:
                 return 0
             res = 1
@@ -44,7 +40,7 @@ class Solution:
                 res = max(res, dfs(child) + 1)
             return res
 
-        return dfs(root)"""
+        return dfs(root)
 
 
         # 方法二：广度优先搜索
@@ -69,13 +65,4 @@ class Solution:
 
 
 
-
-
-
-
-
-
-
-
-
-            
+     
