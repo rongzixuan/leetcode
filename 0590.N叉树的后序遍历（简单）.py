32dec5aa68b12d2d9dcc2be314b8a1812c308fbd
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-13 13:12:50 +0800

    Update and rename 0590.N叉树的后序遍历（简单） to 0590.N叉树的后序遍历（简单）.py

--- 0590.N叉树的后序遍历（简单）.py
+++ 0590.N叉树的后序遍历（简单）.py
@@ -1,9 +1,6 @@
 """
 给定一个 n 叉树的根节点 root ，返回 其节点值的 后序遍历 。
-
 n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
-
- 
 
 示例 1：
 输入：root = [1,null,3,2,4,null,5,6]
@@ -13,7 +10,6 @@ n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由�
 输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
 输出：[2,6,14,11,7,3,12,8,4,13,9,10,5,1]
  
-
 提示：
 节点总数在范围 [0, 10^4] 内
 0 <= Node.val <= 10^4
@@ -39,7 +35,7 @@ class Solution:
         # 方法一：递归
         # 时间复杂度：O(n)
         # 空间复杂度：O(n)
-        """def dfs(root):
+        def dfs(root):
             if not root:
                 return 
             nonlocal ans
@@ -49,13 +45,13 @@ class Solution:
 
         ans = []
         dfs(root)
-        return ans"""
+        return ans
 
 
         # 方法二：迭代（栈）
         # 时间复杂度：O(n)
         # 空间复杂度：O(n)
-        """if not root:
+        if not root:
             return 
 
         from collections import deque
@@ -69,7 +65,7 @@ class Solution:
             for child in root.children:
                 stack.append(child)
 
-        return ans[::-1]"""
+        return ans[::-1]
 
 
         # 方法三：迭代（栈）2
