09c67522061525689739edf57bcb9fb7787a3297
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-13 13:07:55 +0800

    Update and rename 0594.最长和谐子序列（简单） to 0594.最长和谐子序列（简单）.py

--- 0594.最长和谐子序列（简单）.py
+++ 0594.最长和谐子序列（简单）.py
@@ -1,10 +1,7 @@
 """
 和谐数组是指一个数组里元素的最大值和最小值之间的差别 正好是 1 。
-
 现在，给你一个整数数组 nums ，请你在所有可能的子序列中找到最长的和谐子序列的长度。
-
 数组的子序列是一个由数组派生出来的序列，它可以通过删除一些元素或不删除元素、且不改变其余元素的顺序而得到。
-
 
 示例 1：
 输入：nums = [1,3,2,2,5,2,3,7]
@@ -19,9 +16,7 @@
 输入：nums = [1,1,1,1]
 输出：0
  
-
 提示：
-
 1 <= nums.length <= 2 * 104
 -10^9 <= nums[i] <= 10^9
 
@@ -34,7 +29,7 @@ class Solution:
         # 方法一：哈希表
         # 时间复杂度：O(n)
         # 空间复杂度：O(n)
-        """n = len(nums)
+        n = len(nums)
         if n == 1:
             return 0
 
@@ -51,7 +46,7 @@ class Solution:
             #print(max_length)         
 
         #print(hash_table)
-        return max_length"""
+        return max_length
 
 
         # 方法二：排序 + 模拟
