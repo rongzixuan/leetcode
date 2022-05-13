dc201373880b35770ca2fa17998d150bf034cb77
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-13 13:07:09 +0800

    Update and rename 0589.N叉树的前序遍历（简单） to 0589.N叉树的前序遍历（简单）.py

--- 0589.N叉树的前序遍历（简单）.py
+++ 0589.N叉树的前序遍历（简单）.py
@@ -1,8 +1,6 @@
 """
 给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
-
 n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
-
 
 示例 1：
 输入：root = [1,null,3,2,4,null,5,6]
@@ -12,7 +10,6 @@ n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由�
 输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
 输出：[1,2,3,6,7,11,14,4,8,12,5,9,13,10]
  
-
 提示：
 节点总数在范围 [0, 10^4]内
 0 <= Node.val <= 10^4
@@ -38,7 +35,7 @@ class Solution:
         # 方法一：递归
         # 时间复杂度：O(n)
         # 空间复杂度：O(n)
-        """def dfs(root):
+        def dfs(root):
             nonlocal ans
             if not root:
                 return 
@@ -48,7 +45,7 @@ class Solution:
 
         ans = []
         dfs(root)
-        return ans"""
+        return ans
 
 
         # 方法二：栈
