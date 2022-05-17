a713d948227bc52e9efc54da8a2aeb245db7d161
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-17 15:33:53 +0800

    Update and rename 0630.课程表3（困难） to 0630.课程表3（困难）.py

--- 0630.课程表3（困难）.py
+++ 0630.课程表3（困难）.py
@@ -2,9 +2,7 @@
 这里有 n 门不同的在线课程，按从 1 到 n 编号。给你一个数组 courses ，其中 courses[i] = [durationi, lastDayi] 表示第 i 门课将会 持续 上 durationi 天课，并且必须在不晚于 lastDayi 的时候完成。
 
 你的学期从第 1 天开始。且不能同时修读两门及两门以上的课程。
-
 返回你最多可以修读的课程数目。
-
 
 示例 1：
 输入：courses = [[100, 200], [200, 1300], [1000, 1250], [2000, 3200]]
@@ -56,3 +54,6 @@ class Solution:
 
         #print(heap)                       
         return len(heap)
+
+    
+    
