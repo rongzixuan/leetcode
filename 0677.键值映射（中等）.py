92d0808715fa40c7f76101a569e4e1c1b38741f5
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-20 01:10:26 +0800

    Update and rename 0677.键值映射（中等） to 0677.键值映射（中等）.py

--- 0677.键值映射（中等）.py
+++ 0677.键值映射（中等）.py
@@ -5,9 +5,7 @@ MapSum() 初始化 MapSum 对象
 void insert(String key, int val) 插入 key-val 键值对，字符串表示键 key ，整数表示值 val 。如果键 key 已经存在，那么原来的键值对将被替代成新的键值对。
 int sum(string prefix) 返回所有以该前缀 prefix 开头的键 key 的值的总和。
  
-
 示例：
-
 输入：
 ["MapSum", "insert", "sum", "insert", "sum"]
 [[], ["apple", 3], ["ap"], ["app", 2], ["ap"]]
@@ -21,9 +19,7 @@ mapSum.sum("ap");           // return 3 (apple = 3)
 mapSum.insert("app", 2);    
 mapSum.sum("ap");           // return 5 (apple + app = 3 + 2 = 5)
  
-
 提示：
-
 1 <= key.length, prefix.length <= 50
 key 和 prefix 仅由小写英文字母组成
 1 <= val <= 1000
@@ -32,7 +28,7 @@ key 和 prefix 仅由小写英文字母组成
 """
 
 # 方法一：暴力法
-"""class MapSum:
+class MapSum:
     def __init__(self):
         self.map = {}
 
@@ -44,11 +40,11 @@ key 和 prefix 仅由小写英文字母组成
         for k, v in self.map.items():
             if k.startswith(prefix):
                 res += v
-        return res"""
+        return res
 
 
 # 方法二：前缀哈希映射
-"""class MapSum:
+class MapSum:
     def __init__(self):
         self.map = {}
         self.prefixmap = {}
@@ -70,7 +66,7 @@ key 和 prefix 仅由小写英文字母组成
         if prefix in self.prefixmap:
             return self.prefixmap[prefix]
         else:
-            return 0"""
+            return 0
 
 
 # 方法三：字典树
