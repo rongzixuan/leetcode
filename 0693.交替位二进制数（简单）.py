90a98b6da1fe533c5ebe3acd44e9efab5ab9ef51
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-23 17:53:09 +0800

    Update and rename 0693.交替位二进制数（简单）. to 0693.交替位二进制数（简单）.py

--- 0693.交替位二进制数（简单）.py
+++ 0693.交替位二进制数（简单）.py
@@ -1,8 +1,6 @@
 """
 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
 
- 
-
 示例 1：
 输入：n = 5
 输出：true
@@ -17,7 +15,6 @@
 输入：n = 11
 输出：false
 解释：11 的二进制表示是：1011.
- 
 
 提示：
 1 <= n <= 2^31 - 1
@@ -32,7 +29,7 @@ class Solution:
         # 方法一：位运算
         # 时间复杂度：O(logn)
         # 空间复杂度：O(1)
-        """flag = 1 if n & 1 == 0 else 0
+        flag = 1 if n & 1 == 0 else 0
         #print(flag)
 
         while n:
@@ -42,7 +39,7 @@ class Solution:
             flag = (1 - flag)
             n >>= 1
 
-        return True"""
+        return True
 
 
         # 方法二：位运算2
