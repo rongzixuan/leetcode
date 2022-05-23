0b338da921fc7613ee30cf57ceebbe3fe6ace67f
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-23 17:52:11 +0800

    Update and rename 0689.三个无重叠子数组的最大和（困难） to 0689.三个无重叠子数组的最大和（困难）.py

--- 0689.三个无重叠子数组的最大和（困难）.py
+++ 0689.三个无重叠子数组的最大和（困难）.py
@@ -1,9 +1,7 @@
 """
 给你一个整数数组 nums 和一个整数 k ，找出三个长度为 k 、互不重叠、且 3 * k 项的和最大的子数组，并返回这三个子数组。
-
 以下标的数组形式返回结果，数组中的每一项分别指示每个子数组的起始位置（下标从 0 开始）。如果有多个结果，返回字典序最小的一个。
  
-
 示例 1：
 输入：nums = [1,2,1,2,6,7,5,1], k = 2
 输出：[0,3,5]
@@ -14,7 +12,6 @@
 输入：nums = [1,2,1,2,1,2,1,2,1], k = 2
 输出：[0,2,4]
  
-
 提示：
 1 <= nums.length <= 2 * 10^4
 1 <= nums[i] < 2^16
@@ -29,7 +26,7 @@ class Solution:
         # 方法一：前缀和 + 滑动窗口
         # 时间复杂度：O(n)
         # 空间复杂度：O(1)
-        """n = len(nums)
+        n = len(nums)
         tmp_sum1, tmp_sum2, tmp_sum3 = 0, 0, 0
         max_1, max_12, max_123 = 0, 0, 0
         max_1_index, max_12_index, max_123_index = 0, [], []
@@ -55,7 +52,7 @@ class Solution:
             
             #print(max_1_index, max_12_index, max_123_index)
 
-        return max_123_index"""
+        return max_123_index
 
 
         # 方法二：动态规划
