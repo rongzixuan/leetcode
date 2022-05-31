bb334bcfc0d5509babd6ef28b4d7733bb73bf64c
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-05-31 15:51:02 +0800

    Update and rename 0733.图像渲染（简单） to 0733.图像渲染（简单）.py

--- 0733.图像渲染（简单）.py
+++ 0733.图像渲染（简单）.py
@@ -1,11 +1,10 @@
 """
 有一幅以二维整数数组表示的图画，每一个整数表示该图画的像素值大小，数值在 0 到 65535 之间。
-
 给你一个坐标 (sr, sc) 表示图像渲染开始的像素值（行 ，列）和一个新的颜色值 newColor，让你重新上色这幅图像。
-
 为了完成上色工作，从初始坐标开始，记录初始坐标的上下左右四个方向上像素值与初始坐标相同的相连像素点，接着再记录这四个方向上符合条件的像素点与他们对应四个方向上像素值与初始坐标相同的相连像素点，……，重复该过程。将所有有记录的像素点的颜色值改为新的颜色值。
 
 最后返回经过上色渲染后的图像。
+
 """
 
 class Solution:
@@ -15,7 +14,7 @@ class Solution:
         # 方法一：深度优先搜索
         # 时间复杂度:O(m * n)
         # 空间复杂度：O(m * n)，栈的开销
-        """m, n = len(image), len(image[0])
+        m, n = len(image), len(image[0])
         initColor = image[sr][sc]
 
         def dfs(i, j):
@@ -28,7 +27,7 @@ class Solution:
 
         if image[sr][sc] != newColor:
             dfs(sr, sc)
-        return image"""
+        return image
 
 
         # 方法二：广度优先搜索
