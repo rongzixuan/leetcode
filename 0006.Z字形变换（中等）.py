fee037acd78a9cef2192e5701bb422b9a279993c
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-01 15:20:53 +0800

    Update 0006.Z字形变换（中等）.py

--- 0006.Z字形变换（中等）.py
+++ 0006.Z字形变换（中等）.py
@@ -1,6 +1,5 @@
 """
 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
-
 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
 
 P   A   H   N
@@ -9,10 +8,8 @@ Y   I   R
 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
 
 请你实现这个将字符串进行指定行数变换的函数：
-
 string convert(string s, int numRows);
  
-
 示例 1：
 输入：s = "PAYPALISHIRING", numRows = 3
 输出："PAHNAPLSIIGYIR"
@@ -29,7 +26,6 @@ P     I
 示例 3：
 输入：s = "A", numRows = 1
 输出："A"
- 
 
 提示：
 1 <= s.length <= 1000
@@ -73,11 +69,4 @@ class Solution:
 
         return res
         
-
-
-
-
-
-
-              
-
+        
