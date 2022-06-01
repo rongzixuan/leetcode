2701d6c47ecf0b9759cfebed4f099bcc3a2fdfee
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-01 15:21:34 +0800

    Update 0016.最接近的三数之和（中等）.py

--- 0016.最接近的三数之和（中等）.py
+++ 0016.最接近的三数之和（中等）.py
@@ -1,11 +1,7 @@
 """
 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
-
 返回这三个数的和。
-
 假定每组输入只存在恰好一个解。
-
- 
 
 示例 1：
 输入：nums = [-1,2,1,-4], target = 1
@@ -15,7 +11,6 @@
 示例 2：
 输入：nums = [0,0,0], target = 1
 输出：0
- 
 
 提示：
 3 <= nums.length <= 1000
@@ -90,7 +85,6 @@ class Solution:
                 #print('after:', first, second, third)               
 
         return res
-    
     
     
     
