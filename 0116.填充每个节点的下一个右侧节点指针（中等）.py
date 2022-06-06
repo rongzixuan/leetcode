0efc685ba404030ceb7612597fc2d213611122d2
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-06 17:43:01 +0800

    Update 0116.填充每个节点的下一个右侧节点指针（中等）.py

--- 0116.填充每个节点的下一个右侧节点指针（中等）.py
+++ 0116.填充每个节点的下一个右侧节点指针（中等）.py
@@ -1,15 +1,15 @@
 """
 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
-
 struct Node {
   int val;
   Node *left;
   Node *right;
   Node *next;
 }
-填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
 
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
 初始状态下，所有 next 指针都被设置为 NULL。
+
 """
 
 """
@@ -49,11 +49,4 @@ class Solution:
         return root
 
 
-
-
-
-
-
-
-
-        
+     
