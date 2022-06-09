95a6cd19e899bd4743a77748d3bad671aa0116fa
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-09 12:35:38 +0800

    Update 0187.重复的DNA序列（中等）.py

--- 0187.重复的DNA序列（中等）.py
+++ 0187.重复的DNA序列（中等）.py
@@ -1,6 +1,5 @@
 """
 所有 DNA 都由一系列缩写为 'A'，'C'，'G' 和 'T' 的核苷酸组成，例如："ACGAATTCCG"。在研究 DNA 时，识别 DNA 中的重复序列有时会对研究非常有帮助。
-
 编写一个函数来找出所有目标子串，目标子串的长度为 10，且在 DNA 字符串 s 中出现次数超过一次。
 
 """
@@ -36,7 +35,6 @@ class Solution:
         return list(res)
 
 
-
         # 方法二：哈希表 + 滑动窗口 + 位运算
         # 时间复杂度：O(n)
         # 空间复杂度：O(n)
@@ -61,9 +59,4 @@ class Solution:
                 res.append(s[i: i + 10])
 
         return res
-
-
-
-
-
 
