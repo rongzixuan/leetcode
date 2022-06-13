534f9bfbfa204ecbf8940e19545e973fea50a0c8
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-14 06:09:06 +0800

    Update 0211.添加与搜索单词-数据结构设计（中等）.py

--- 0211.添加与搜索单词-数据结构设计（中等）.py
+++ 0211.添加与搜索单词-数据结构设计（中等）.py
@@ -2,7 +2,6 @@
 请你设计一个数据结构，支持 添加新单词 和 查找字符串是否与任何先前添加的字符串匹配 。
 
 实现词典类 WordDictionary ：
-
 WordDictionary() 初始化词典对象
 void addWord(word) 将 word 添加到数据结构中，之后可以对它进行匹配
 bool search(word) 如果数据结构中存在字符串与 word 匹配，则返回 true ；否则，返回  false 。word 中可能包含一些 '.' ，每个 . 都可以表示任何一个字母。
@@ -59,3 +58,4 @@ class WordDictionary:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
+
