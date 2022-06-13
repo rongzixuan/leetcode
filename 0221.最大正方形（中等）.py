750a8c0992c170ab7696ed48cc0ad12410c425a3
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-14 06:10:53 +0800

    Update 0221.最大正方形（中等）.py

--- 0221.最大正方形（中等）.py
+++ 0221.最大正方形（中等）.py
@@ -1,5 +1,6 @@
 """
 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+
 """
 
 class Solution:
@@ -50,7 +51,5 @@ class Solution:
                 max_length = max(max_length, min(j-i+1, width))
 
         return max_length * max_length
-    
-    
     
     
