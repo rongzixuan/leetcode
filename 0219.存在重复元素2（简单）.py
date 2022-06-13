dc6f276f61b22adaf61290766f87918bfdc7745c
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-14 06:10:10 +0800

    Update 0219.存在重复元素2（简单）.py

--- 0219.存在重复元素2（简单）.py
+++ 0219.存在重复元素2（简单）.py
@@ -1,7 +1,6 @@
 """
 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
 
-
 示例 1：
 输入：nums = [1,2,3,1], k = 3
 输出：true
@@ -13,7 +12,6 @@
 示例 3：
 输入：nums = [1,2,3,1,2,3], k = 2
 输出：false
- 
  
 提示：
 1 <= nums.length <= 10^5
@@ -78,11 +76,5 @@ class Solution:
             right += 1
 
         return False
-
-
-
-
-
-
 
 
