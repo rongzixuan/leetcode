248171bbaf1c1a1b06afb57de07d1fd2959d7e2f
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-19 06:07:00 +0800

    Update 0231.2的幂（简单）.py

--- 0231.2的幂（简单）.py
+++ 0231.2的幂（简单）.py
@@ -1,6 +1,5 @@
 """
 给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
-
 如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。
 
 """
@@ -36,9 +35,3 @@ class Solution:
         return True if (n & (n-1)) == 0 else False
 
         
-
-
-
-
-
-
