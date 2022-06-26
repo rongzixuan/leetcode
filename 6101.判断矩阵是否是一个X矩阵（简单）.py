8b9b5142c6a5b08cc9c0c586a18163b50b6aef3c
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-26 15:18:36 +0800

    Create 6101.判断矩阵是否是一个X矩阵（简单）.py

---
+++ 6101.判断矩阵是否是一个X矩阵（简单）.py
@@ -0,0 +1,55 @@
+"""
+如果一个正方形矩阵满足下述 全部 条件，则称之为一个 X 矩阵 ：
+矩阵对角线上的所有元素都 不是 0
+矩阵中所有其他元素都是 0
+
+给你一个大小为 n x n 的二维整数数组 grid ，表示一个正方形矩阵。如果 grid 是一个 X 矩阵 ，返回 true ；否则，返回 false 。
+
+示例 1：
+输入：grid = [[2,0,0,1],[0,3,1,0],[0,5,2,0],[4,0,0,2]]
+输出：true
+解释：矩阵如上图所示。
+X 矩阵应该满足：绿色元素（对角线上）都不是 0 ，红色元素都是 0 。
+因此，grid 是一个 X 矩阵。
+
+示例 2：
+输入：grid = [[5,7,0],[0,3,1],[0,5,0]]
+输出：false
+解释：矩阵如上图所示。
+X 矩阵应该满足：绿色元素（对角线上）都不是 0 ，红色元素都是 0 。
+因此，grid 不是一个 X 矩阵。
+ 
+提示：
+n == grid.length == grid[i].length
+3 <= n <= 100
+0 <= grid[i][j] <= 10^5
+
+"""
+
+class Solution:
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        
+        
+        # 方法一：模拟
+        # 时间复杂度：O(n ^ 2)
+        # 空间复杂度：O(1)
+        n = len(grid)
+        total = sum(grid[i][j] for i in range(n) for j in range(n))
+        #print('total:', total)
+        
+        tmp_total = 0
+        for i in range(n):
+            if grid[i][i] == 0:
+                #print(i, i)
+                return False
+            if grid[i][n - 1 - i] == 0:
+                #print(i, n - 1 - i)
+                return False
+            tmp_total += grid[i][i]
+            if i != n - 1 - i:
+                tmp_total += grid[i][n - 1 - i]
+        #print('tmp_total:', tmp_total)
+            
+        return tmp_total == total
+        
+        
