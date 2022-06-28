d7e53f24e05c83d32749a128fd8b5c7f82ab23a9
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-28 22:39:27 +0800

    Update 0313.超级丑数（中等）.py

--- 0313.超级丑数（中等）.py
+++ 0313.超级丑数（中等）.py
@@ -1,6 +1,5 @@
 """
 超级丑数 是一个正整数，并满足其所有质因数都出现在质数数组 primes 中。
-
 给你一个整数 n 和一个整数数组 primes ，返回第 n 个 超级丑数 。
 
 题目数据保证第 n 个 超级丑数 在 32-bit 带符号整数范围内
@@ -29,7 +28,6 @@ class Solution:
         return ugly
 
 
-
         # 方法二：动态规划
         # 时间复杂度：O(nm)
         # 空间复杂度：O(n+m)
@@ -51,9 +49,5 @@ class Solution:
 
         #print(dp)
         return dp[n]
-
-
-
-
 
 
