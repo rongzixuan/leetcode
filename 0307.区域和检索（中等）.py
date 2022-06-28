a90860c74aac3dbe23d4085e95f2d5355772786b
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-06-28 22:37:31 +0800

    Update 0307.区域和检索（中等）.py

--- 0307.区域和检索（中等）.py
+++ 0307.区域和检索（中等）.py
@@ -3,13 +3,12 @@
 
 其中一类查询要求 更新 数组 nums 下标对应的值
 另一类查询要求返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 ，其中 left <= right
-实现 NumArray 类：
 
+实现 NumArray 类：
 NumArray(int[] nums) 用整数数组 nums 初始化对象
 void update(int index, int val) 将 nums[index] 的值 更新 为 val
 int sumRange(int left, int right) 返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 （即，nums[left] + nums[left + 1], ..., nums[right]）
  
-
 示例 1：
 
 输入：
@@ -24,7 +23,6 @@ numArray.sumRange(0, 2); // 返回 1 + 3 + 5 = 9
 numArray.update(1, 2);   // nums = [1,2,5]
 numArray.sumRange(0, 2); // 返回 1 + 2 + 5 = 8
  
-
 提示：
 1 <= nums.length <= 3 * 10^4
 -100 <= nums[i] <= 100
