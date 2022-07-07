2361e5d7e8c331a7add77b6f1126dec92069dbe4
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-07 15:06:27 +0800

    Update 0335.路径交叉（困难）.py

--- 0335.路径交叉（困难）.py
+++ 0335.路径交叉（困难）.py
@@ -1,12 +1,10 @@
 """
 给你一个整数数组 distance 。
-
 从 X-Y 平面上的点 (0,0) 开始，先向北移动 distance[0] 米，然后向西移动 distance[1] 米，向南移动 distance[2] 米，向东移动 distance[3] 米，持续移动。也就是说，每次移动后你的方位会发生逆时针变化。
 
 判断你所经过的路径是否相交。如果相交，返回 true ；否则，返回 false 。
 
 """
-
 
 class Solution:
     def isSelfCrossing(self, distance: List[int]) -> bool:
@@ -69,13 +67,6 @@ class Solution:
             i += 1
 
         return i != n
-
-
-
-
-
-
-
 
 
 
