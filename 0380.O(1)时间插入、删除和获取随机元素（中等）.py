203ca8093ce94af22f1122260badce0325a13a64
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-10 14:59:51 +0800

    Update 0380.O(1)时间插入、删除和获取随机元素（中等）.py

--- 0380.O(1)时间插入、删除和获取随机元素（中等）.py
+++ 0380.O(1)时间插入、删除和获取随机元素（中等）.py
@@ -1,12 +1,11 @@
 """
 实现RandomizedSet 类：
-
 RandomizedSet() 初始化 RandomizedSet 对象
 bool insert(int val) 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，返回 false 。
 bool remove(int val) 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，返回 false 。
 int getRandom() 随机返回现有集合中的一项（测试用例保证调用此方法时集合中至少存在一个元素）。每个元素应该有 相同的概率 被返回。
-你必须实现类的所有函数，并满足每个函数的 平均 时间复杂度为 O(1) 。
 
+你必须实现类的所有函数，并满足每个函数的 平均 时间复杂度为 O(1) 。
 
 示例：
 
@@ -26,7 +25,6 @@ randomizedSet.remove(1); // 从集合中移除 1 ，返回 true 。集合现在�
 randomizedSet.insert(2); // 2 已在集合中，所以返回 false 。
 randomizedSet.getRandom(); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
  
-
 提示：
 -2^31 <= val <= 2^31 - 1
 最多调用 insert、remove 和 getRandom 函数 2 * 10^5 次
@@ -73,3 +71,5 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+
