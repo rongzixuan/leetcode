1204a522711709a66db3953bb83ca073b3f69303
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-10 15:08:00 +0800

    Update 0383.赎金信（简单）.py

--- 0383.赎金信（简单）.py
+++ 0383.赎金信（简单）.py
@@ -1,12 +1,8 @@
 """
 为了不在赎金信中暴露字迹，从杂志上搜索各个需要的字母，组成单词来表达意思。
-
 给你一个赎金信 (ransomNote) 字符串和一个杂志(magazine)字符串，判断 ransomNote 能不能由 magazines 里面的字符构成。
-
 如果可以构成，返回 true ；否则返回 false 。
-
 magazine 中的每个字符只能在 ransomNote 中使用一次。
-
 
 示例 1：
 输入：ransomNote = "a", magazine = "b"
@@ -20,7 +16,6 @@ magazine 中的每个字符只能在 ransomNote 中使用一次。
 输入：ransomNote = "aa", magazine = "aab"
 输出：true
  
-
 提示：
 1 <= ransomNote.length, magazine.length <= 10^5
 ransomNote 和 magazine 由小写英文字母组成
@@ -46,3 +41,6 @@ class Solution:
             count[ord(ransomNote[i]) - ord('a')] -= 1
 
         return True
+
+    
+    
