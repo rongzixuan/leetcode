97132097c7211d17cf0b0b284b20a1bc26235ee0
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-10 15:09:29 +0800

    Update 0385.迷你语法分析器（中等）.py

--- 0385.迷你语法分析器（中等）.py
+++ 0385.迷你语法分析器（中等）.py
@@ -1,9 +1,6 @@
 """
 给定一个字符串 s 表示一个整数嵌套列表，实现一个解析它的语法分析器并返回解析的结果 NestedInteger 。
-
 列表中的每个元素只可能是整数或整数嵌套列表
-
- 
 
 示例 1：
 输入：s = "324",
@@ -20,7 +17,6 @@
     ii. 一个包含一个元素的嵌套列表
          a. 一个 integer 包含值 789
  
-
 提示：
 1 <= s.length <= 5 * 10^4
 s 由数字、方括号 "[]"、负号 '-' 、逗号 ','组成
@@ -28,7 +24,6 @@ s 由数字、方括号 "[]"、负号 '-' 、逗号 ','组成
 输入中的所有值的范围是 [-10^6, 10^6]
 
 """
-
 
 # """
 # This is the interface that allows for creating nested lists.
@@ -72,6 +67,7 @@ s 由数字、方括号 "[]"、负号 '-' 、逗号 ','组成
 #        Return None if this NestedInteger holds a single integer
 #        :rtype List[NestedInteger]
 #        """
+
 class Solution:
     def deserialize(self, s: str) -> NestedInteger:
 
@@ -136,3 +132,5 @@ class Solution:
         return stack.pop()
 
 
+    
+    
