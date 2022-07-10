722ff96d495e8eb99b26b0127c23b6144c01937c
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-10 15:08:36 +0800

    Update 0384.打乱数组（中等）.py

--- 0384.打乱数组（中等）.py
+++ 0384.打乱数组（中等）.py
@@ -2,12 +2,12 @@
 给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。
 
 实现 Solution class:
-
 Solution(int[] nums) 使用整数数组 nums 初始化对象
 int[] reset() 重设数组到它的初始状态并返回
 int[] shuffle() 返回数组随机打乱后的结果
  
 示例：
+
 输入
 ["Solution", "shuffle", "reset", "shuffle"]
 [[[1, 2, 3]], [], [], []]
@@ -20,7 +20,6 @@ solution.shuffle();    // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]�
 solution.reset();      // 重设数组到它的初始状态 [1, 2, 3] 。返回 [1, 2, 3]
 solution.shuffle();    // 随机返回数组 [1, 2, 3] 打乱后的结果。例如，返回 [1, 3, 2]
  
-
 提示：
 1 <= nums.length <= 200
 -10^6 <= nums[i] <= 10^6
@@ -83,3 +82,5 @@ class Solution:
 # obj = Solution(nums)
 # param_1 = obj.reset()
 # param_2 = obj.shuffle()
+
+
