0553cfcc1d622fc6348e970265a6b2585761d66b
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-12 19:22:35 +0800

    Update 0432.全O(1)的数据结构（困难）.py

--- 0432.全O(1)的数据结构（困难）.py
+++ 0432.全O(1)的数据结构（困难）.py
@@ -2,16 +2,13 @@
 请你设计一个用于存储字符串计数的数据结构，并能够返回计数最小和最大的字符串。
 
 实现 AllOne 类：
-
 AllOne() 初始化数据结构的对象。
 inc(String key) 字符串 key 的计数增加 1 。如果数据结构中尚不存在 key ，那么插入计数为 1 的 key 。
 dec(String key) 字符串 key 的计数减少 1 。如果 key 的计数在减少后为 0 ，那么需要将这个 key 从数据结构中删除。测试用例保证：在减少计数前，key 存在于数据结构中。
 getMaxKey() 返回任意一个计数最大的字符串。如果没有元素存在，返回一个空字符串 "" 。
 getMinKey() 返回任意一个计数最小的字符串。如果没有元素存在，返回一个空字符串 "" 。
- 
 
 示例：
-
 输入
 ["AllOne", "inc", "inc", "getMaxKey", "getMinKey", "inc", "getMaxKey", "getMinKey"]
 [[], ["hello"], ["hello"], [], [], ["leet"], [], []]
@@ -28,7 +25,6 @@ allOne.inc("leet");
 allOne.getMaxKey(); // 返回 "hello"
 allOne.getMinKey(); // 返回 "leet"
  
-
 提示：
 1 <= key.length <= 10
 key 由小写英文字母组成
@@ -114,3 +110,5 @@ class AllOne:
 # obj.dec(key)
 # param_3 = obj.getMaxKey()
 # param_4 = obj.getMinKey()
+
+
