16433e9415ab2b1007dde1172c2bc9f56c173044
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-12 19:25:16 +0800

    Update 0437.路径总和3（中等）.py

--- 0437.路径总和3（中等）.py
+++ 0437.路径总和3（中等）.py
@@ -1,6 +1,5 @@
 """
 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
-
 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
 
 """
@@ -14,6 +13,7 @@
 class Solution:   
     def pathSum(self, root: TreeNode, targetSum: int) -> int:
 
+        
         # 方法一：深度优先搜索
         # 时间复杂度：O(n * depth)
         # 空间复杂度：O(n)
@@ -47,7 +47,6 @@ class Solution:
         return res
 
 
-
         # 方法二：深度优先搜索
         # 时间复杂度：O(n^2)
         # 空间复杂度：O(n)，栈的开销
@@ -69,7 +68,6 @@ class Solution:
         res += self.pathSum(root.left, targetSum)
         res += self.pathSum(root.right, targetSum)
         return res
-
 
 
         # 方法三：前缀和
@@ -101,3 +99,6 @@ class Solution:
         return res
 
 
+
+    
+    
