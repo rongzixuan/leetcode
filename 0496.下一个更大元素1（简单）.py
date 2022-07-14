2e4ac83e1c21032e5917e6989a15f1189d2f784d
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-07-14 15:35:11 +0800

    Update 0496.下一个更大元素1（简单）.py

--- 0496.下一个更大元素1（简单）.py
+++ 0496.下一个更大元素1（简单）.py
@@ -1,6 +1,5 @@
 """
 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
-
 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
 
 nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
@@ -48,12 +47,4 @@ class Solution:
         return [res[num] for num in nums1]
 
 
-
-
-
-
-
-
-
-            
 
