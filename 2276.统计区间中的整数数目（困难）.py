aa6931f53bd30f5bdb5731412226463b48989839
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-08-04 10:52:19 +0800

    Update and rename 6066.统计区间中的整数数目（困难）.py to 2276.统计区间中的整数数目（困难）.py

--- 2276.统计区间中的整数数目（困难）.py
+++ 2276.统计区间中的整数数目（困难）.py
@@ -1,16 +1,14 @@
 """
 给你区间的 空 集，请你设计并实现满足要求的数据结构：
-
 新增：添加一个区间到这个区间集合中。
 统计：计算出现在 至少一个 区间中的整数个数。
-实现 CountIntervals 类：
 
+实现 CountIntervals 类：
 CountIntervals() 使用区间的空集初始化对象
 void add(int left, int right) 添加区间 [left, right] 到区间集合之中。
 int count() 返回出现在 至少一个 区间中的整数个数。
-注意：区间 [left, right] 表示满足 left <= x <= right 的所有整数 x 。
 
- 
+注意：区间 [left, right] 表示满足 left <= x <= right 的所有整数 x 。 
 
 示例 1：
 
@@ -68,3 +66,5 @@ class CountIntervals:
 # obj = CountIntervals()
 # obj.add(left,right)
 # param_2 = obj.count()
+
+
