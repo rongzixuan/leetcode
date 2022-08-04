fc423383c61a2b4ef87d5e8ed111713a28c75f86
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2022-08-04 10:54:02 +0800

    Update and rename 6064.不含特殊楼层的最大连续楼层数（中等）.py to 2274.不含特殊楼层的最大连续楼层数（中等）.py

--- 2274.不含特殊楼层的最大连续楼层数（中等）.py
+++ 2274.不含特殊楼层的最大连续楼层数（中等）.py
@@ -1,11 +1,8 @@
 """
 Alice 管理着一家公司，并租用大楼的部分楼层作为办公空间。Alice 决定将一些楼层作为 特殊楼层 ，仅用于放松。
-
 给你两个整数 bottom 和 top ，表示 Alice 租用了从 bottom 到 top（含 bottom 和 top 在内）的所有楼层。另给你一个整数数组 special ，其中 special[i] 表示  Alice 指定用于放松的特殊楼层。
-
 返回不含特殊楼层的 最大 连续楼层数。
  
-
 示例 1：
 输入：bottom = 2, top = 9, special = [4,6]
 输出：3
