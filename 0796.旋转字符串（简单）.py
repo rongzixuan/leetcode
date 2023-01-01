10aaedcb2580a6006bea77ba41113a512582c818
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-01-01 22:19:10 +0800

    Update and rename 0796.旋转字符串（简单） to 0796.旋转字符串（简单）.py

--- 0796.旋转字符串（简单）.py
+++ 0796.旋转字符串（简单）.py
@@ -5,7 +5,6 @@ s 的 旋转操作 就是将 s 最左边的字符移动到最右边。 
 
 例如, 若 s = 'abcde'，在旋转一次之后结果就是'bcdea' 。
  
-
 示例 1:
 输入: s = "abcde", goal = "cdeab"
 输出: true
@@ -13,7 +12,6 @@ s 的 旋转操作 就是将 s 最左边的字符移动到最右边。 
 示例 2:
 输入: s = "abcde", goal = "abced"
 输出: false
- 
 
 提示:
 1 <= s.length, goal.length <= 100
@@ -39,3 +37,7 @@ class Solution:
                     return True
 
         return False
+
+    
+    
+    
