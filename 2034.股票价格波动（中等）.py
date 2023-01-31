4c62ffd5327d92877595055f69921b3bea5a993d
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-01-31 11:57:21 +0800

    Update and rename 2034.股票价格波动（中等） to 2034.股票价格波动（中等）.py

--- 2034.股票价格波动（中等）.py
+++ 2034.股票价格波动（中等）.py
@@ -4,19 +4,17 @@
 不巧的是，由于股票市场内在的波动性，股票价格记录可能不是按时间顺序到来的。某些情况下，有的记录可能是错的。如果两个有相同时间戳的记录出现在数据流中，前一条记录视为错误记录，后出现的记录 更正 前一条错误的记录。
 
 请你设计一个算法，实现：
-
 更新 股票在某一时间戳的股票价格，如果有之前同一时间戳的价格，这一操作将 更正 之前的错误价格。
 找到当前记录里 最新股票价格 。最新股票价格 定义为时间戳最晚的股票价格。
 找到当前记录里股票的 最高价格 。
 找到当前记录里股票的 最低价格 。
-请你实现 StockPrice 类：
 
+请你实现 StockPrice 类：
 StockPrice() 初始化对象，当前无股票价格记录。
 void update(int timestamp, int price) 在时间点 timestamp 更新股票价格为 price 。
 int current() 返回股票 最新价格 。
 int maximum() 返回股票 最高价格 。
 int minimum() 返回股票 最低价格 。
- 
 
 示例 1：
 
@@ -38,7 +36,6 @@ stockPrice.maximum();     // 返回 5 ，更正后最高价格为 5 。
 stockPrice.update(4, 2);  // 时间戳为 [1,2,4] ，对应价格为 [3,5,2] 。
 stockPrice.minimum();     // 返回 2 ，最低价格时间戳为 4 ，价格为 2 。
  
-
 提示：
 1 <= timestamp, price <= 10^9
 update，current，maximum 和 minimum 总 调用次数不超过 10^5 。
@@ -54,7 +51,7 @@ current，maximum 和 minimum 被调用时，update 操作 至少 已经被
 # maximum(): O(1)
 # minimum(): O(1)
 # 空间复杂度：O(n)
-"""from sortedcontainers import SortedList
+from sortedcontainers import SortedList
 class StockPrice:
 
     def __init__(self):
@@ -77,7 +74,7 @@ class StockPrice:
         return self.price[-1]
 
     def minimum(self) -> int:
-        return self.price[0]"""
+        return self.price[0]
 
 
 # 方法二：哈希表 + 2个优先队列
