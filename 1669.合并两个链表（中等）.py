865e586e314a188e93d9aaa6a12c2fed9cb2630c
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-01-31 11:44:13 +0800

    Create 1669.合并两个链表（中等）.py

---
+++ 1669.合并两个链表（中等）.py
@@ -0,0 +1,67 @@
+"""
+给你两个链表 list1 和 list2 ，它们包含的元素分别为 n 个和 m 个。
+
+请你将 list1 中下标从 a 到 b 的全部节点都删除，并将list2 接在被删除节点的位置。
+
+下图中蓝色边和节点展示了操作后的结果：
+
+请你返回结果链表的头指针。
+
+示例 1：
+输入：list1 = [0,1,2,3,4,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+输出：[0,1,2,1000000,1000001,1000002,5]
+解释：我们删除 list1 中下标为 3 和 4 的两个节点，并将 list2 接在该位置。上图中蓝色的边和节点为答案链表。
+
+示例 2：
+输入：list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+输出：[0,1,1000000,1000001,1000002,1000003,1000004,6]
+解释：上图中蓝色的边和节点为答案链表。
+ 
+提示：
+3 <= list1.length <= 10^4
+1 <= a <= b < list1.length - 1
+1 <= list2.length <= 10^4
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+
+
+        # 方法一：模拟
+        # 时间复杂度：O(n + m)
+        # 空间复杂度：O(1)
+        #print(list1, list2)
+        tmp_a = list1
+        pre = tmp_a
+        count = 0
+        while tmp_a.next and count != a:
+            pre = tmp_a
+            tmp_a = tmp_a.next
+            count += 1
+
+        tmp_b = tmp_a
+        while tmp_b.next and count != b:
+            tmp_b = tmp_b.next
+            count += 1
+        nxt = tmp_b.next
+
+        tmp_c = list2
+        pre.next = list2
+        while tmp_c and tmp_c.next:
+            tmp_c = tmp_c.next
+        
+        tmp_c.next = nxt
+        return list1
+
+
+
+
+
+
+
