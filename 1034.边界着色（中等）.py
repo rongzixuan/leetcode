250a3cf7d314a2b452e68eef829a66a61a849422
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-07 17:37:00 +0800

    Update and rename 1034.边界着色（中等） to 1034.边界着色（中等）.py

--- 1034.边界着色（中等）.py
+++ 1034.边界着色（中等）.py
@@ -2,16 +2,14 @@
 给你一个大小为 m x n 的整数矩阵 grid ，表示一个网格。另给你三个整数 row、col 和 color 。网格中的每个值表示该位置处的网格块的颜色。
 
 两个网格块属于同一 连通分量 需满足下述全部条件：
-
 两个网格块颜色相同
 在上、下、左、右任意一个方向上相邻
-连通分量的边界 是指连通分量中满足下述条件之一的所有网格块：
 
+连通分量的边界 是指连通分量中满足下述条件之一的所有网格块：
 在上、下、左、右四个方向上与不属于同一连通分量的网格块相邻
 在网格的边界上（第一行/列或最后一行/列）
+
 请你使用指定颜色 color 为所有包含网格块 grid[row][col] 的 连通分量的边界 进行着色，并返回最终的网格 grid 。
-
-
 
 示例 1：
 输入：grid = [[1,1],[1,2]], row = 0, col = 0, color = 3
@@ -25,7 +23,6 @@
 输入：grid = [[1,1,1],[1,1,1],[1,1,1]], row = 1, col = 1, color = 2
 输出：[[2,2,2],[2,1,2],[2,2,2]]
  
-
 提示：
 m == grid.length
 n == grid[i].length
@@ -44,7 +41,7 @@ class Solution:
         # 方法一：广度优先搜索
         # 时间复杂度：O(m * n)
         # 空间复杂度：O(m * n)
-        """m, n = len(grid), len(grid[0])
+        m, n = len(grid), len(grid[0])
         init_color = grid[row][col]
         if init_color == color:
             return grid
@@ -64,7 +61,7 @@ class Solution:
                         visited[new_row][new_col] = True
                         queue.append((new_row, new_col))
 
-        return grid"""
+        return grid
 
 
         # 方法二：深度优先搜索
