ef899d91c5e48e5ad7e77660caac81ef0589846b
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-07 17:45:03 +0800

    Update and rename 1078.Bigram分词（简单） to 1078.Bigram分词（简单）.py

--- 1078.Bigram分词（简单）.py
+++ 1078.Bigram分词（简单）.py
@@ -2,7 +2,6 @@
 给出第一个词 first 和第二个词 second，考虑在某些文本 text 中可能以 "first second third" 形式出现的情况，其中 second 紧随 first 出现，third 紧随 second 出现。
 
 对于每种这样的情况，将第三个词 "third" 添加到答案中，并返回答案。
-
  
 示例 1：
 输入：text = "alice is a good girl she is a good student", first = "a", second = "good"
@@ -40,3 +39,6 @@ class Solution:
                 res.append(arr[k+2])
 
         return res
+
+    
+    
