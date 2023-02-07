4eb843f56b92910491883144353ba65782542c04
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-07 17:48:24 +0800

    Update and rename 1380.矩阵中的幸运数（简单） to 1380.矩阵中的幸运数（简单）.py

--- 1380.矩阵中的幸运数（简单）.py
+++ 1380.矩阵中的幸运数（简单）.py
@@ -2,10 +2,8 @@
 给你一个 m * n 的矩阵，矩阵中的数字 各不相同 。请你按 任意 顺序返回矩阵中的所有幸运数。
 
 幸运数是指矩阵中满足同时下列两个条件的元素：
-
 在同一行的所有元素中最小
 在同一列的所有元素中最大
- 
 
 示例 1：
 输入：matrix = [[3,7,8],[9,11,13],[15,16,17]]
@@ -21,7 +19,6 @@
 输入：matrix = [[7,8],[1,2]]
 输出：[7]
  
-
 提示：
 m == mat.length
 n == mat[i].length
@@ -39,13 +36,13 @@ class Solution:
         # 方法一：模拟
         # 时间复杂度：O(m * n)
         # 空间复杂度：O(1)
-        """res = []
+        res = []
         for row in matrix:
             #print(row)
             for j, x in enumerate(row):
                 if x == min(row) == max(r[j] for r in matrix):
                     res.append(x)
-        return res"""
+        return res
 
 
         # 方法二：模拟2
