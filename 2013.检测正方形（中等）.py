0313cfb4aee1f83791927e95ae0e55b33b974f82
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-09 17:01:00 +0800

    Update and rename 2013.检测正方形（中等） to 2013.检测正方形（中等）.py

--- 2013.检测正方形（中等）.py
+++ 2013.检测正方形（中等）.py
@@ -1,16 +1,14 @@
 """
 给你一个在 X-Y 平面上的点构成的数据流。设计一个满足下述要求的算法：
-
 添加 一个在数据流中的新点到某个数据结构中。可以添加 重复 的点，并会视作不同的点进行处理。
 给你一个查询点，请你从数据结构中选出三个点，使这三个点和查询点一同构成一个 面积为正 的 轴对齐正方形 ，统计 满足该要求的方案数目。
+
 轴对齐正方形 是一个正方形，除四条边长度相同外，还满足每条边都与 x-轴 或 y-轴 平行或垂直。
 
 实现 DetectSquares 类：
-
 DetectSquares() 使用空数据结构初始化对象
 void add(int[] point) 向数据结构添加一个新的点 point = [x, y]
 int count(int[] point) 统计按上述方式与点 point = [x, y] 共同构造 轴对齐正方形 的方案数。
- 
 
 示例：
 
@@ -33,7 +31,6 @@ detectSquares.count([11, 10]); // 返回 2 。你可以选择：
                                //   - 第一个，第二个，和第三个点
                                //   - 第一个，第三个，和第四个点
  
-
 提示：
 point.length == 2
 0 <= x, y <= 1000
@@ -71,3 +68,4 @@ class DetectSquares:
 # obj = DetectSquares()
 # obj.add(point)
 # param_2 = obj.count(point)
+
