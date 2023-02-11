0ea1037b23b0b4074aa9cf507f784e010f871d8b
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-11 11:17:47 +0800

    Update and rename 2038.如果相邻两个颜色均相同则删除当前颜色（中等） to 2038.如果相邻两个颜色均相同则删除当前颜色（中等）.py

--- 2038.如果相邻两个颜色均相同则删除当前颜色（中等）.py
+++ 2038.如果相邻两个颜色均相同则删除当前颜色（中等）.py
@@ -6,10 +6,10 @@ Alice 和 Bob 在玩一个游戏，他们 轮流 从这个字符串中删除颜
 如果一个颜色片段为 'A' 且 相邻两个颜色 都是颜色 'A' ，那么 Alice 可以删除该颜色片段。Alice 不可以 删除任何颜色 'B' 片段。
 如果一个颜色片段为 'B' 且 相邻两个颜色 都是颜色 'B' ，那么 Bob 可以删除该颜色片段。Bob 不可以 删除任何颜色 'A' 片段。
 Alice 和 Bob 不能 从字符串两端删除颜色片段。
-如果其中一人无法继续操作，则该玩家 输 掉游戏且另一玩家 获胜 。
-假设 Alice 和 Bob 都采用最优策略，如果 Alice 获胜，请返回 true，否则 Bob 获胜，返回 false。
 
- 
+如果其中一人无法继续操作，则该玩家 输 掉游戏且另一玩家 获胜 。
+
+假设 Alice 和 Bob 都采用最优策略，如果 Alice 获胜，请返回 true，否则 Bob 获胜，返回 false。
 
 示例 1：
 输入：colors = "AAABABB"
@@ -46,7 +46,6 @@ ABBBBBBBAA -> ABBBBBBAA
 然后轮到 Alice 操作，她无法删除任何片段。
 所以 Bob 获胜，返回 false 。
  
-
 提示：
 1 <= colors.length <= 10^5
 colors 只包含字母 'A' 和 'B'
