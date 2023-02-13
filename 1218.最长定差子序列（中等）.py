8d67c117cdde9482d6fc22f808243fa6fcb63a67
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-13 17:25:10 +0800

    Update and rename 1218.最长定差子序列（中等） to 1218.最长定差子序列（中等）.py

--- 1218.最长定差子序列（中等）.py
+++ 1218.最长定差子序列（中等）.py
@@ -3,27 +3,22 @@
 
 子序列 是指在不改变其余元素顺序的情况下，通过删除一些元素或不删除任何元素而从 arr 派生出来的序列。
 
- 
-
 示例 1：
-
 输入：arr = [1,2,3,4], difference = 1
 输出：4
 解释：最长的等差子序列是 [1,2,3,4]。
-示例 2：
 
+示例 2：
 输入：arr = [1,3,5,7], difference = 1
 输出：1
 解释：最长的等差子序列是任意单个元素。
-示例 3：
 
+示例 3：
 输入：arr = [1,5,7,8,5,3,4,2,1], difference = -2
 输出：4
 解释：最长的等差子序列是 [7,5,3,1]。
  
-
 提示：
-
 1 <= arr.length <= 10^5
 -10^4 <= arr[i], difference <= 10^4
 
@@ -36,7 +31,7 @@ class Solution:
         # 方法一：动态规划 + 哈希表
         # 时间复杂度：O(n)
         # 空间复杂度：O(n)
-        """n = len(arr)
+        n = len(arr)
         if n == 1:
             return 1
 
@@ -49,7 +44,7 @@ class Solution:
             hash_table[arr[i]].append(i)
         #print(hash_table)
 
-        return max(dp)"""
+        return max(dp)
 
 
         # 方法二：动态规划 + 哈希表
@@ -65,3 +60,7 @@ class Solution:
 
         return max(dp.values())
 
+
+    
+    
+    
