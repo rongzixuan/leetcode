d6510c5fbcb1f073a1ee175b73dcbbbbdd555f0f
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-13 17:24:15 +0800

    Update and rename 1189.“气球”的最大数量（简单） to 1189.“气球”的最大数量（简单）.py

--- 1189.“气球”的最大数量（简单）.py
+++ 1189.“气球”的最大数量（简单）.py
@@ -3,8 +3,6 @@
 
 字符串 text 中的每个字母最多只能被使用一次。请你返回最多可以拼凑出多少个单词 "balloon"。
 
- 
-
 示例 1：
 输入：text = "nlaebolko"
 输出：1
@@ -17,7 +15,6 @@
 输入：text = "leetcode"
 输出：0
  
-
 提示：
 1 <= text.length <= 10^4
 text 全部由小写英文字母组成
