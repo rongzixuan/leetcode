b868b6c2dcdb84a8617d5327f65b4db21eaf39dd
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-17 22:26:11 +0800

    Update and rename 1646.获取生成数组中的最大值（简单） to 1646.获取生成数组中的最大值（简单）.py

--- 1646.获取生成数组中的最大值（简单）.py
+++ 1646.获取生成数组中的最大值（简单）.py
@@ -1,10 +1,10 @@
 """
 给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
-
 nums[0] = 0
 nums[1] = 1
 当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
 当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
+
 返回生成数组 nums 中的 最大 值。
 
 """
@@ -29,4 +29,8 @@ class Solution:
             max_num = max(max_num, nums[i])
 
         return max_num
+        
+
+        
+        
         
