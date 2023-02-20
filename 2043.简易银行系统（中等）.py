ef221c5c9d334b2898910821d0b4248d7358a811
rongzixuan <34224853+rongzixuan@users.noreply.github.com>
2023-02-20 18:07:36 +0800

    Update and rename 2043.简易银行系统（中等） to 2043.简易银行系统（中等）.py

--- 2043.简易银行系统（中等）.py
+++ 2043.简易银行系统（中等）.py
@@ -2,17 +2,15 @@
 你的任务是为一个很受欢迎的银行设计一款程序，以自动化执行所有传入的交易（转账，存款和取款）。银行共有 n 个账户，编号从 1 到 n 。每个账号的初始余额存储在一个下标从 0 开始的整数数组 balance 中，其中第 (i + 1) 个账户的初始余额是 balance[i] 。
 
 请你执行所有 有效的 交易。如果满足下面全部条件，则交易 有效 ：
-
 指定的账户数量在 1 和 n 之间，且
 取款或者转账需要的钱的总数 小于或者等于 账户余额。
-实现 Bank 类：
 
+实现 Bank 类：
 Bank(long[] balance) 使用下标从 0 开始的整数数组 balance 初始化该对象。
 boolean transfer(int account1, int account2, long money) 从编号为 account1 的账户向编号为 account2 的账户转帐 money 美元。如果交易成功，返回 true ，否则，返回 false 。
 boolean deposit(int account, long money) 向编号为 account 的账户存款 money 美元。如果交易成功，返回 true ；否则，返回 false 。
 boolean withdraw(int account, long money) 从编号为 account 的账户取款 money 美元。如果交易成功，返回 true ；否则，返回 false 。
  
-
 示例：
 
 输入：
@@ -33,7 +31,6 @@ bank.transfer(3, 4, 15); // 返回 false ，账户 3 的当前余额是 $10 。
                          // 所以无法转账 $15 。
 bank.withdraw(10, 50);   // 返回 false ，交易无效，因为账户 10 并不存在。
  
-
 提示：
 n == balance.length
 1 <= n, account, account1, account2 <= 10^5
@@ -75,3 +72,5 @@ class Bank:
 # param_1 = obj.transfer(account1,account2,money)
 # param_2 = obj.deposit(account,money)
 # param_3 = obj.withdraw(account,money)
+
+
